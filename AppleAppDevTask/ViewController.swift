76102fc793a28e867be0acb3f7b8c6ca2cecb4da
Pavel Krivtsov <krivcov.pavel@gmail.com>
2023-05-11 21:21:10 +0300

    Initial Commit

---
+++ AppleAppDevTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleAppDevTask
+//
+//  Created by Pavel Krivtsov on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
