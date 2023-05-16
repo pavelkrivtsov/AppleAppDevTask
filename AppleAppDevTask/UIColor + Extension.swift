30af7b7d38841cac9ba8cd848434ed70a0152121
Pavel Krivtsov <krivcov.pavel@gmail.com>
2023-05-16 16:12:58 +0300

    Fix  extension

--- AppleAppDevTask/UIColor + Extension.swift
+++ AppleAppDevTask/UIColor + Extension.swift
@@ -1,5 +1,5 @@
 //
-//  Extension+UIColor.swift
+//  UIColor + Extension.swift
 //  AppleAppDevTask
 //
 //  Created by Pavel Krivtsov on 12.05.2023.
@@ -8,15 +8,7 @@
 import UIKit
 
 public extension UIColor {
-    
-    //    convenience init(hex: Int, alpha: CGFloat = 1.0) {
-    //        let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
-    //        let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
-    //        let blue = CGFloat((hex & 0xFF)) / 255.0
-    //
-    //        self.init(red: red, green: green, blue: blue, alpha: alpha)
-    //    }
-    
+        
     convenience init(hex string: String, alpha: CGFloat = 1.0) {
         var hex = string.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
         
@@ -66,42 +58,3 @@ public extension UIColor {
         }
     }
 }
-
-
-
-//{
-//    "company": {
-//        "companyId": "99df1b98-2861-4ed2-93c4-f8e3c5c18ce7"
-//    },
-//    "customerMarkParameters": {
-//        "loyaltyLevel": {
-//            "number": 7,
-//            "name": "платиновый",
-//            "requiredSum": 10823,
-//            "markToCash": 93,
-//            "cashToMark": 68
-//        },
-//        "mark": 496
-//    },
-//    "mobileAppDashboard": {
-//        "companyName": "Венская кофейня",
-//        "logo": "http://bonusmoney.info/image/mail/bm.png",
-//        "backgroundColor": "#EFEFEF",
-//        "mainColor": "#2688EB",
-//        "cardBackgroundColor": "#FFFFFF",
-//        "textColor": "#949494",
-//        "highlightTextColor": "#1A1A1A",
-//        "accentColor": "#FF3044"
-//    }
-//}
-
-//"mobileAppDashboard": {
-//    "companyName": "У тебя может получиться!",
-//    "logo": "http://bonusmoney.info/image/mail/bm.png",
-//    "backgroundColor": "#4E63A6",
-//    "mainColor": "#66FC73",
-//    "cardBackgroundColor": "#8F9341",
-//    "textColor": "#237186",
-//    "highlightTextColor": "#0E529B",
-//    "accentColor": "#4014F8"
-//}
